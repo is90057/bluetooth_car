3e052ebb9a3acf92d793e5f54753114104162fa4
JamesChang <is90057@gmail.com>
2022-02-15 09:48:40 +0800

    feat:us-100

--- sketch_jan16a/sketch_jan16a.ino
+++ sketch_jan16a/sketch_jan16a.ino
@@ -1,7 +1,11 @@
-// 藍牙控制 L298N Arduino 小車
-// 藍牙使用 HC-05 or HC-06 for Android 安卓手機
-// 安卓手機 APP 請至 Play Store 下載 “ Arduino Bluetooth” by CircuitMagic
-// 如您使用 iPhone手機 藍牙請用HM-10; 手機 APP 請至 APP Store 下載任何可藉由藍牙發送字元的應用程式
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(3, 2);
+
+unsigned int HighByte = 0;
+unsigned int LowByte  = 0;
+unsigned int Len  = 0;
+
 int spd=180;             //設定速度值 0-255
 int turnspd = spd-60;    //當車子轉彎時，有一個輪子速度較慢
   
@@ -9,49 +13,87 @@ const int IN1 = 10;      // L298N IN1 連接 Arduino pin 10
 const int IN2 = 11;      // L298N IN2 連接 Arduino pin 11
 const int IN3 = 5;       // L298N IN3 連接 Arduino pin 5
 const int IN4 = 6;       // L298N IN4 連接 Arduino pin 6
+int models = 0;
 
-void setup() 
-{
-  Serial.begin(57600);   //確認藍牙傳輸速率 9600 bits/second
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+
   pinMode(5,OUTPUT);    // Arduino 輸出電壓控制車子
   pinMode(6,OUTPUT);
   pinMode(11,OUTPUT);
   pinMode(10,OUTPUT);
 }
- 
-void loop() 
-{
-  if (Serial.available()) {
-  char c = Serial.read();     //讀入手機App藉由藍牙輸入的字元, 字元存入 c
-  Serial.println(c);          //在序列埠監視器中印出所讀入的字元 c
- 
-    switch(c){
-    case '5':                //當接收到的字元為5，車子前進
-      Forward();
-    break;
-    
-    case '7':               //當接收到的字元為7，車子後退
-      Back();
-    break;
-    
-    case '1':               //當接收到的字元為1，車子左轉
-      Left();
-    break;
-    
-    case '3':              //當接收到的字元為3，車子右轉
-      Right();
-    break;
 
-    case '0':
-      Stop();
-    
-    default:               //當所接收到的字元非以上所述，車子停止
-      Stop();
-    break;
-  }
-}
-  delay(100);
-}
+void loop() {
+    char c;
+    if (Serial.available()){
+        c = Serial.read();     //讀入手機App藉由藍牙輸入的字元, 字元存入 c
+        Serial.println(c);          //在序列埠監視器中印出所讀入的字元 c
+        if(c=='2'){
+            models = 0;
+        }else if(c=='4'){
+            models =1;
+            
+        }  
+    }
+    if(models==0){
+        //Serial.println(models);          //在序列埠監視器中印出所讀入的字元 c
+        switch(c){
+            case '5':                //當接收到的字元為5，車子前進
+            Forward();
+            break;
+            
+            case '7':               //當接收到的字元為7，車子後退
+            Back();
+            break;
+            
+            case '1':               //當接收到的字元為1，車子左轉
+            Left();
+            break;
+            
+            case '3':              //當接收到的字元為3，車子右轉
+            Right();
+            break;
+
+            case '0':
+            Stop();
+            
+            default:               //當所接收到的字元非以上所述，車子停止
+            Stop();
+            break;
+        }
+ 
+    }else if(models==1){
+        Forward();
+        mySerial.flush();
+        mySerial.write(0X55);                           // trig US-100 begin to measure the distance
+        delay(500);                                  
+        if (mySerial.available() >= 200)                  // check receive 2 bytes correctly
+        {
+            HighByte = mySerial.read();
+            LowByte  = mySerial.read();
+            Len  = HighByte * 256 + LowByte;          // Calculate the distance
+            Serial.println(Len); 
+            if ((Len > 1) && (Len < 10000))
+            {
+                if (Len >200){
+                    // 先倒退
+                    for(int i=0;i<5000; i++){
+                        Back();
+                    }
+                    // 再右轉
+                    for(int i=0;i<5000; i++){
+                        Right();
+                    }
+                }               
+            }
+        }
+
+    }
+ // delay(300);                                    
+} 
 
 void Forward(void)        //車子前進子程式
 {
